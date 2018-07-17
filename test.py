71da802e448639e2495c14e394ca67b343cf445c
Jacob Dannhausen-Brun <jdannhausenbrun@gmail.com>
2018-07-16 23:22:12 -0500

    Added check for reponse status code

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ from constants import API_KEY
 URL = "http://api.sportradar.us/mlb/trial/v6.5/en/games/d4f1ecee-8318-42e7-8ce9-90411d089dfa/summary.json?api_key=%s" %(API_KEY)
 
 r = requests.get(url = URL)
-
-data = r.json()
-
-print(data)
+if r.status_code != 200:
+    print("Error Reading Data")
+else:
+    print(r.json())
